0280730418c0f12cfd125a7dee8eed1d7fb849c3
Diz138 <mattdzwil@gmail.com>
2025-03-23 21:47:24 -0400

    reverting png change for lowercase .png images

--- js/detailing.js
+++ js/detailing.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
         "./images/detailing/detailing5.PNG", "./images/detailing/detailing6.PNG",
         "./images/detailing/detailing1.PNG", "./images/detailing/detailing8.PNG",
         "./images/detailing/detailing9.PNG", "./images/detailing/detailing10.PNG",
-        "./images/detailing/detailing11.PNG", "./images/detailing/detailing12.PNG",
-        "./images/detailing/detailing13.PNG", "./images/detailing/detailing14.PNG",
-        "./images/detailing/detailing15.PNG", "./images/detailing/detailing16.PNG"
+        "./images/detailing/detailing11.png", "./images/detailing/detailing12.png",
+        "./images/detailing/detailing13.png", "./images/detailing/detailing14.png",
+        "./images/detailing/detailing15.png", "./images/detailing/detailing16.png"
     ];
     function populateGallery(sectionId, images) {
         const gallery = document.getElementById(sectionId);
--- js/windowTint.js
+++ js/windowTint.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const windowTintImages = [
         "./images/windowTint/windowTint1.PNG", "./images/windowTint/windowTint2.PNG",
-        "./images/windowTint/windowTint3.PNG", "./images/windowTint/windowTint4.PNG",
+        "./images/windowTint/windowTint3.PNG", "./images/windowTint/windowTint4.png",
         "./images/windowTint/windowTint5.PNG", "./images/windowTint/windowTint6.PNG",
         "./images/windowTint/windowTint7.PNG", "./images/windowTint/windowTint8.PNG"
     ];

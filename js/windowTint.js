1f9e90b224ebf133942586f7281ff4cbda315f8f
Diz138 <mattdzwil@gmail.com>
2025-03-23 20:41:10 -0400

    Updating images with case sensitive .PNG

--- js/windowTint.js
+++ js/windowTint.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const windowTintImages = [
-        "images/windowTint/windowTint1.png", "images/windowTint/windowTint2.png",
-        "images/windowTint/windowTint3.png", "images/windowTint/windowTint4.png",
-        "images/windowTint/windowTint5.png", "images/windowTint/windowTint6.png",
-        "images/windowTint/windowTint7.png", "images/windowTint/windowTint8.png"
+        "./images/windowTint/windowTint1.png", "./images/windowTint/windowTint2.png",
+        "./images/windowTint/windowTint3.png", "./images/windowTint/windowTint4.png",
+        "./images/windowTint/windowTint5.png", "./images/windowTint/windowTint6.png",
+        "./images/windowTint/windowTint7.png", "./images/windowTint/windowTint8.png"
     ];
     function populateGallery(sectionId, images) {
         const gallery = document.getElementById(sectionId);
